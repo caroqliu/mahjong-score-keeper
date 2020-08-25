e204a07f977004866196ba3dcba7602f59e09aa4
caroqliu <carolineqliu@gmail.com>
2020-08-24 22:48:08 -0400

    Persist player names across `restart`

--- src/App.js
+++ src/App.js
@@ -6,16 +6,16 @@ const DEFAULT_PLAYER_NAMES = ['East', 'South', 'West', 'North'];
 
 function App() {
   const [points, setPoints] = useState(400)
-  const [chips, setChips] = useState(null)
+  const [gameInProgress, setGameInProgress] = useState(false)
   const names = useRef(DEFAULT_PLAYER_NAMES.slice(0))
   const restart = () => {
-    names.current = DEFAULT_PLAYER_NAMES.slice(0);
-    setChips(null);
+    setGameInProgress(false);
   };
   return (
     <div className="App">
       <header className="App-header">
-        { chips == null ?  
+        { gameInProgress ? 
+        <Game chips={chips} players={names.current} onRestart={restart}/> :
         <>
           <h1>
             Let's play Mahjong!
@@ -26,8 +26,8 @@ function App() {
           <p>Who is playing?<br />
           {names.current.map((name, i) => <PlayerName key={'player-' + name} dir={name} index={i} players={names}/>)}
           </p>
-          <button className="start-game" onClick={() => setChips(points)}>Start</button>
-        </>: <Game chips={chips} players={names.current} onRestart={restart}/>}
+          <button className="start-game" onClick={() => setGameInProgress(true)}>Start</button>
+        </>}
       </header>
     </div>
   );
